4c8ee6c089db14919f07e2c9d25347ff7ac6b71a
MINIMANIMO <99541984+loveryanmuch@users.noreply.github.com>
2023-11-01 21:47:45 -0700

    Update Regression Modeling.py

--- Regression Modeling.py
+++ Regression Modeling.py
@@ -1,10 +1,7 @@
 import statsmodels.api as sm
 
-# Add a constant to the predictor variable
-X = sm.add_constant(df['Number_of_EV_users'])
 
-# Y is the response variable
+X = sm.add_constant(df['Number_of_EV_users'])
 Y = df['Number_of_charging_stations']
 
-# Fit the regression model
 model = sm.OLS(Y, X).fit()
