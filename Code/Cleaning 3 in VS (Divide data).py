60a72fa676fcbe30874d71d8a4f6a56f5fcab959
Zoey Kim <zoooey96@gmail.com>
2023-11-03 09:34:33 -0700

    organize our data and file names

---
+++ Code/Cleaning 3 in VS (Divide data).py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+# Reading the Excel file
+df_income = pd.read_excel("C:\\Users\\17789\\LHL\\EV charging in Washington state\\data\\Income and Poverty Estimates for U.S. States and Counties.xlsx")
+
+# Data where 'indicator' starts with 'AGE'
+df_age = df_income[df_income['indicator'].str.startswith('AGE')]
+
+# Data where 'indicator' starts with 'SEX'
+df_sex = df_income[df_income['indicator'].str.startswith('SEX')]
+
+# Data where 'indicator' starts with 'RACE'
+df_race = df_income[df_income['indicator'].str.startswith('RACE')]
+
+# Saving the dataframes to CSV files
+df_age.to_csv("C:\\Users\\17789\\LHL\\EV charging in Washington state\\data\\Cleaned_data\\Population by age.csv", index=False)
+df_sex.to_csv("C:\\Users\\17789\\LHL\\EV charging in Washington state\\data\\Cleaned_data\\Population by sex.csv", index=False)
+df_race.to_csv("C:\\Users\\17789\\LHL\\EV charging in Washington state\\data\\Cleaned_data\\Population by race.csv", index=False)
